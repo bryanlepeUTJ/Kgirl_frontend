94b87808cb29b2cca920119eee57871cad82da02
Bryan Lepe <bryan.lepe10@gmail.com>
2022-03-10 15:32:16 -0600

    modulo de ventas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './common/menu/menu.component';
+import { VentasComponent } from './ventas/ventas.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    VentasComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
