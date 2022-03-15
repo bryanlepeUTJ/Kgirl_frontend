4005165812c8002a592ec9d2b999df09b3b70e18
Bryan Lepe <bryan.lepe10@gmail.com>
2022-03-14 19:25:20 -0600

    ajustes login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { MenuComponent } from './common/menu/menu.component';
 
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: MenuComponent }
+
   /*{
     path: 'auth',
     loadChildren: () => import ('./auth/auth.module').then( m => m.AuthModule)
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,8 +5,6 @@ import { LoginComponent } from './pages/login/login.component';
 import { ForgotComponent } from './pages/forgot/forgot.component';
 import { AuthRoutingModule } from './auth-routing.module';
 
-
-
 @NgModule({
   declarations: [
     RegistroComponent,
@@ -16,6 +14,7 @@ import { AuthRoutingModule } from './auth-routing.module';
   imports: [
     CommonModule,
     AuthRoutingModule
-  ]
+  ], 
+  exports: [LoginComponent]
 })
 export class AuthModule { }
